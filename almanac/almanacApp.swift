75dbd82757c17e1703c72d608411188b168e268c
hugoPeyronCode <hugopeyron@gmail.com>
2025-05-28 21:52:46 +0200

    Initial Commit

---
+++ almanac/almanacApp.swift
@@ -0,0 +1,17 @@
+//
+//  almanacApp.swift
+//  almanac
+//
+//  Created by Hugo Peyron on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct almanacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
